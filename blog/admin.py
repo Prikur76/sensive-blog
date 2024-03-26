73df2203d80e42cc0c23d704f19872d75074487f
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2024-03-26 11:17:58 +0300

    Fix admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -4,12 +4,20 @@ from blog.models import Post, Tag, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'slug', 'author', 'text', 'image', 'published_at']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ['published_at', 'author']
+    search_fields = ['title', 'text']
+    date_hierarchy = 'published_at'
+    raw_id_fields = ['author', 'likes', 'tags']
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['post', 'author', 'text', 'published_at']
+    list_filter = ['published_at']
+    search_fields = ['text']
+    date_hierarchy = 'published_at'
 
 
 @admin.register(Tag)
