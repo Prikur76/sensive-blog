5433b7e209d681b474d898ae8544bc0a04e192eb
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2024-04-02 15:58:07 +0300

    Fix static_root

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -42,7 +42,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = ''
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 TEMPLATES = [
     {
